88073beb8d9a39996e9b908bbfdc767953c70836
Daniel Angell <daniel@copy.ai>
2021-06-28 11:11:47 -1000

    Add README.md

--- spiral.mjs
+++ spiral.mjs
@@ -11,7 +11,7 @@ export const fragment = (x, y, time) => {
   const distFromCenter = distance([x, y], [0.5, 0.5]);
   const angle = Math.atan2(y - 0.5, x - 0.5) + time;
 
-  const bandSize = 0.05;
+  const bandSize = 0.2;
   // prettier-ignore
   const bandLocation = ((distFromCenter + bandSize * (angle / 2 * Math.PI)) / bandSize) % 2;
   if (bandLocation >= 1) return 0;
